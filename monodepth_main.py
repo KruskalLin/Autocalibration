505ac96cb9a1d3cf65eafd0dbd27e802fcb6e47b
LimKruscal <161250070@smail.nju.edu.cn>
2019-12-08 16:44:44 +0800

    commit monodepth

---
+++ monodepth_main.py
@@ -0,0 +1,196 @@
+import argparse
+import time
+import torch
+import numpy as np
+import torch.optim as optim
+from tqdm import tqdm
+# custom modules
+
+from loss import MonodepthLoss
+from utils import get_model, to_device, prepare_dataloader
+
+# plot params
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.rcParams['figure.figsize'] = (15, 10)
+
+
+def return_arguments():
+    parser = argparse.ArgumentParser(description='PyTorch Monodepth')
+
+    parser.add_argument('--data_dir',
+                        help='path to the dataset folder. \
+                        It should contain subfolders with following structure:\
+                        "image_02/data" for left images and \
+                        "image_03/data" for right images'
+                        )
+    parser.add_argument('--val_data_dir',
+                        help='path to the validation dataset folder. \
+                            It should contain subfolders with following structure:\
+                            "image_02/data" for left images and \
+                            "image_03/data" for right images'
+                        )
+    parser.add_argument('--model_path', help='path to the trained model')
+    parser.add_argument('--output_directory',
+                        help='where save dispairities\
+                        for tested images'
+                        )
+    parser.add_argument('--input_height', type=int, help='input height',
+                        default=256)
+    parser.add_argument('--input_width', type=int, help='input width',
+                        default=512)
+    parser.add_argument('--model', default='resnet18_md',
+                        help='encoder architecture: ' +
+                        'resnet18_md or resnet50_md ' + '(default: resnet18)'
+                        + 'or torchvision version of any resnet model'
+                        )
+    parser.add_argument('--pretrained', default=False,
+                        help='Use weights of pretrained model'
+                        )
+    parser.add_argument('--mode', default='test',
+                        help='mode: train or test (default: test)')
+    parser.add_argument('--epochs', default=50,
+                        help='number of total epochs to run')
+    parser.add_argument('--learning_rate', default=1e-4,
+                        help='initial learning rate (default: 1e-4)')
+    parser.add_argument('--batch_size', default=256,
+                        help='mini-batch size (default: 256)')
+    parser.add_argument('--adjust_lr', default=True,
+                        help='apply learning rate decay or not\
+                        (default: True)'
+                        )
+    parser.add_argument('--device',
+                        default='cuda:0',
+                        help='choose cpu or cuda:0 device"'
+                        )
+    parser.add_argument('--do_augmentation', default=True,
+                        help='do augmentation of images or not')
+    parser.add_argument('--augment_parameters', default=[
+        0.8,
+        1.2,
+        0.5,
+        2.0,
+        0.8,
+        1.2,
+        ],
+            help='lowest and highest values for gamma,\
+                        brightness and color respectively'
+            )
+    parser.add_argument('--print_images', default=False,
+                        help='print disparity and image\
+                        generated from disparity on every iteration'
+                        )
+    parser.add_argument('--print_weights', default=False,
+                        help='print weights of every layer')
+    parser.add_argument('--input_channels', default=3,
+                        help='Number of channels in input tensor')
+    parser.add_argument('--num_workers', default=4,
+                        help='Number of workers in dataloader')
+    parser.add_argument('--use_multiple_gpu', default=False)
+    args = parser.parse_args()
+    return args
+
+
+def adjust_learning_rate(optimizer, epoch, learning_rate):
+    """Sets the learning rate to the initial LR\
+        decayed by 2 every 10 epochs after 30 epoches"""
+
+    if epoch >= 30 and epoch < 40:
+        lr = learning_rate / 2
+    elif epoch >= 40:
+        lr = learning_rate / 4
+    else:
+        lr = learning_rate
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+def post_process_disparity(disp):
+    (_, h, w) = disp.shape
+    l_disp = disp[0, :, :]
+    r_disp = np.fliplr(disp[1, :, :])
+    m_disp = 0.5 * (l_disp + r_disp)
+    (l, _) = np.meshgrid(np.linspace(0, 1, w), np.linspace(0, 1, h))
+    l_mask = 1.0 - np.clip(20 * (l - 0.05), 0, 1)
+    r_mask = np.fliplr(l_mask)
+    return r_mask * l_disp + l_mask * r_disp + (1.0 - l_mask - r_mask) * m_disp
+
+
+class Model:
+
+    def __init__(self, args):
+        self.args = args
+
+        # Set up model
+        self.device = args.device
+        self.model = get_model(args.model, input_channels=args.input_channels, pretrained=args.pretrained)
+        self.model = self.model.to(self.device)
+        if args.use_multiple_gpu:
+            self.model = torch.nn.DataParallel(self.model)
+
+        self.model.load_state_dict(torch.load(args.model_path))
+        args.augment_parameters = None
+        args.do_augmentation = False
+        args.batch_size = 1
+
+        # Load data
+        self.output_directory = args.output_directory
+        self.input_height = args.input_height
+        self.input_width = args.input_width
+
+        self.n_img, self.loader = prepare_dataloader(args.data_dir, args.mode, args.augment_parameters,
+                                                     args.do_augmentation, args.batch_size,
+                                                     (args.input_height, args.input_width),
+                                                     args.num_workers)
+
+
+        if 'cuda' in self.device:
+            torch.cuda.synchronize()
+
+
+
+    def save(self, path):
+        torch.save(self.model.state_dict(), path)
+
+    def load(self, path):
+        self.model.load_state_dict(torch.load(path))
+
+    def test(self):
+        self.model.eval()
+        disparities = np.zeros((self.n_img,
+                               self.input_height, self.input_width),
+                               dtype=np.float32)
+        disparities_pp = np.zeros((self.n_img,
+                                  self.input_height, self.input_width),
+                                  dtype=np.float32)
+        print('prepare_pre_depth...')
+        with torch.no_grad():
+            for (i, data) in tqdm(enumerate(self.loader)):
+                # Get the inputs
+                data = to_device(data, self.device)
+                left = data.squeeze()
+                # Do a forward pass
+                disps = self.model(left)
+                disp = disps[0][:, 0, :, :].unsqueeze(1)
+                disparities[i] = disp[0].squeeze().cpu().numpy()
+                disparities_pp[i] = \
+                    post_process_disparity(disps[0][:, 0, :, :]\
+                                           .cpu().numpy())
+
+        np.save(self.output_directory + '/disparities.npy', disparities)
+        np.save(self.output_directory + '/disparities_pp.npy',
+                disparities_pp)
+        print('Finished Testing')
+
+
+def main():
+    args = return_arguments()
+    if args.mode == 'test':
+        model_test = Model(args)
+        model_test.test()
+
+
+if __name__ == '__main__':
+    main()
+
